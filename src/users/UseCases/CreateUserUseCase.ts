1eaf6dce95e9d438208f3d170ce3efef3d67b033
willen <willen.sesantos@gmail.com>
2022-10-29 18:24:33 -0300

    chore: Valida entrada de dados com @hapi/joi

--- src/users/UseCases/CreateUserUseCase.ts
+++ src/users/UseCases/CreateUserUseCase.ts
@@ -1,10 +1,18 @@
 import { User } from "../entities/User";
 import { IUser } from "../protocols/User";
+const Joi = require("@hapi/joi");
 
 export class CreateUserUseCase {
   constructor(private userRepository: IUser) {}
 
   async create(user: User): Promise<void> {
+    const fields = Joi.object({
+      username: Joi.required(),
+      password: Joi.required(),
+    });
+
+    await fields.validateAsync(user);
+
     const verifyUserExists = await this.userRepository.find(user.username);
     if (verifyUserExists != null) throw "user already exists";
 
